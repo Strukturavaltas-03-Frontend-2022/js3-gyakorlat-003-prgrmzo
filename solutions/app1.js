346f5dcfd2d576d5912b2d0666b3459b4612d731
Zo <pahyzoard@outlook.hu>
2022-08-31 13:44:53 +0200

    mission completed

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,1 +1,9 @@
+const setCookie = (string) => {
+  const expirationMinutes = 15;
+  const expires = new Date(
+    Date.now() + expirationMinutes * 60 * 1000
+  ).toUTCString();
+  document.cookie = `token=${string};expires=${expires}`;
+};
 
+export default setCookie;
